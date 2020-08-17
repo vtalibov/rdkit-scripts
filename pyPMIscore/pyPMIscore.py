8e7e61c4b70ef36192a267ff167820a656765e49
vtalibov <vladimir.o.talibov@gmail.com>
2020-08-17 16:09:10 +0200

    Average of sum of normalized principal moments of interia

---
+++ pyPMIscore/pyPMIscore.py
@@ -0,0 +1,51 @@
+from rdkit import Chem
+from rdkit.Chem import AllChem, Descriptors3D
+
+
+def embed_confs(mol, seed=0):
+    ''' Embeds multiple conformations to a molecule,
+    returns a list of conformation IDs
+    '''
+    conformers = AllChem.EmbedMultipleConfs(mol, pruneRmsThresh=0.5, numConfs=27, randomSeed=seed)
+    return list(conformers)
+
+
+def mmff_opt_confs(mol, seed=0):
+    ''' Performes MMFF'94 optiomization of embed conformations,
+    returns convergence and absolute energy value.
+    '''
+    embed_confs(mol, seed)
+    energies = Chem.rdForceFieldHelpers.MMFFOptimizeMoleculeConfs(mol)
+    return mol, energies
+
+
+def converged_conformers(mol, energies, cutoff=10):
+    '''Purges unconverged FF-optimized conformers,
+    returns mol and a table with conformer ID and its
+     energy relative to the most lost energy conformer.
+    (rdchem.Mol, [(confID, energy),...])
+    '''
+    conf_n_energies = list()
+    for i in range(len(energies)):
+        if energies[i][0] == 0 and energies[i][1] - min(energies)[1] < cutoff:
+            conf_n_energies.append((i, energies[i][1] - min(energies)[1]))
+        else:
+            mol.RemoveConformer(i)
+    return mol, conf_n_energies
+
+
+def average_PMIscore(mol, cutoff=10):
+    mol = mmff_opt_confs(mol)
+    mol = converged_conformers(mol[0], mol[1])
+    NPR1 = list()
+    NPR2 = list()
+    for i in range(mol[0].GetNumConformers()):
+        mol[0].ClearComputedProps() # RDKit version 2017.09.1, not needed in newer versions
+        NPR1.append(Descriptors3D.NPR1(mol[0], mol[1][i][0]))
+        NPR2.append(Descriptors3D.NPR2(mol[0], mol[1][i][0]))
+    return (sum(NPR1)+sum(NPR2))/len(NPR1)
+
+
+if __name__ == '__main__':
+    import sys
+    print(average_PMIscore(Chem.AddHs(Chem.MolFromSmiles(sys.argv[1]))))
